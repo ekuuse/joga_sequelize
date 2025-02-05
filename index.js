a4b04c5c6c4055a3e907718ef41fcfaf9bb7c505
ekuuse <erki.kuuse@voco.ee>
2025-02-05 08:51:46 +0200

    connected to the database

--- index.js
+++ index.js
@@ -4,6 +4,18 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
+const Sequelize = require("sequelize")
+const sequelize = new Sequelize('mysql://root:qwerty@localhost:3306/joga_sequelize')
+
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log("db connected")
+    })
+    .catch(err => {
+        console.error("db not connected: ", err)
+    })
+
 app.get("/", (req,res) => {
     res.json({message: "welcome"})
 })
